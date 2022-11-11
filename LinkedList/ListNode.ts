1d277759abd8571751866d31cc18135849e45dd7
Gustavo Antonio <gustavo1998antonio@gmail.com>
2022-11-11 12:03:07 -0300

    add prev support in list node

--- LinkedList/ListNode.ts
+++ LinkedList/ListNode.ts
@@ -1,9 +1,11 @@
 export class ListNode {
     value: any;
     next: any;
+    prev: any;
 
     constructor(value: any) {
         this.value = value;
         this.next = null;
+        this.prev = null;
     }
 }
